be934c659967a498681fbf408794a26a6f11af3c
Mrinal <mail.mrinal10@gmail.com>
2020-12-25 23:24:06 +0530

    First commit

---
+++ src/main/java/com/interviewpoint/backend/InterviewPointApplication.java
@@ -0,0 +1,13 @@
+package com.interviewpoint.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewPointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterviewPointApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/interviewpoint/backend/InterviewPointApplicationTests.java
@@ -0,0 +1,13 @@
+package com.interviewpoint.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewPointApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
