f04673006234b634b164f0ad87029de3960141ee
Mrinal <mail.mrinal10@gmail.com>
2020-12-25 23:54:20 +0530

    adding required controllers

---
+++ src/main/java/com/interviepoint/backend/controller/LoginController.java
@@ -0,0 +1,21 @@
+package com.interviepoint.backend.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoginController {
+	@RequestMapping(value="/userLogin")
+	public void userLogin() {
+		
+	}
+	
+	public void interviewerLogin() {
+		
+	}
+
+	public void adminLogin() {
+		
+	}
+	
+}
